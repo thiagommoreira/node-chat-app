5cfc5cf9546365eb29899b295085d0fe4e7f86fe
Thiago <thiagofoz@gmail.com>
2018-01-16 10:57:18 -0200

    Creating the connected and disconnected message, server and client side

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 const path = require('path');
 const hbs = require('hbs');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 //hbs.registerPartials(__dirname+'/views/partials');
 
 app.set('view engine', 'hbs');
 
 app.use(express.static(publicPath));
 
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Port ${PORT} is where the magic happens.`);
 });
