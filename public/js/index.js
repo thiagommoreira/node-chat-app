7664e8d308cd06e336f4bf4e4dd5984478a1b5aa
Thiago <thiagofoz@gmail.com>
2018-01-19 15:04:40 -0200

    Added scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom() {
+   var messages = jQuery('#messages');
+   var newMessage = messages.children('li:last-child');
+
+   var clientHeight = messages.prop('clientHeight');
+   var scrollTop = messages.prop('scrollTop');
+   var scrollHeight = messages.prop('scrollHeight');
+   var newMessageHeight = newMessage.innerHeight();
+   var lastMessageHeight = newMessage.prev().innerHeight();
+
+   if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+   }
+}
+
 socket.on('connect', function() {
     console.log('connected to server');
 
@@ -14,11 +29,12 @@ socket.on('newMessage', function(m) {
     var template = jQuery('#message-template').html();
     var html = Mustache.render(template, {
       text: m.text,
-      time: formattedTime,
-      user: m.from
+      createdAT: formattedTime,
+      from: m.from
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
     //
     // var li = jQuery('<li></li>');
     // li.text(`${formattedTime} ${m.from}: ${m.text}`);
@@ -36,7 +52,7 @@ socket.on('newLocationMessage', function(m) {
   });
   console.log(html);
   jQuery('#messages').append(html);
-
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">My current Location</a>');
   // li.text(`${formattedTime} ${m.from}:`);
